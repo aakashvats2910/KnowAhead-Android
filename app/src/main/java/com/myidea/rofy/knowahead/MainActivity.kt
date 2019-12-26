016c98041bf1cb64b5bd840a9b178c04a0987b33
Aakash Sharma <aakashvats2910@gmail.com>
2019-12-26 14:15:13 +0530

    Everything started up again with new idead!

--- app/src/main/java/com/myidea/rofy/knowahead/MainActivity.kt
+++ app/src/main/java/com/myidea/rofy/knowahead/MainActivity.kt
@@ -13,17 +13,15 @@ import androidx.annotation.Px
 import com.google.firebase.database.FirebaseDatabase
 import com.myidea.rofy.knowahead.firebase.AppFirebase
 import com.myidea.rofy.knowaheadlibrary.UserLib.AppUser
+import kotlin.reflect.typeOf
 
 class MainActivity : AppCompatActivity() {
 
     // Variables for the widgets on the screen.
-    lateinit var main_layout : LinearLayout
-    lateinit var username_text_field: EditText
-    lateinit var password_text_field: EditText
-    lateinit var show_password_check_box: CheckBox
-
-    // Private variables for this class.
-    lateinit var a: ViewGroup.LayoutParams
+    lateinit var main_layout: LinearLayout
+    lateinit var first_text_view: TextView
+    lateinit var second_text_view: TextView
+    lateinit var use_it_button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,36 +29,31 @@ class MainActivity : AppCompatActivity() {
 
         // Initialization of the widgets.
         main_layout = LinearLayout(applicationContext)
-        username_text_field = EditText(applicationContext)
-        password_text_field = EditText(applicationContext)
-        show_password_check_box = CheckBox(applicationContext)
+        first_text_view = TextView(applicationContext)
+        second_text_view = TextView(applicationContext)
+        use_it_button = Button(applicationContext)
 
-        // Setting up the orientation of the main_layout to vertical!
+        // Setting orientation of main_layout
         main_layout.orientation = LinearLayout.VERTICAL
 
-        // Adding the widgets in the
-        main_layout.addView(username_text_field, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT ))
-        main_layout.addView(password_text_field, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT ))
-        main_layout.addView(show_password_check_box)
+        // Adding characteristics to first_text_view
+        first_text_view.text = "No need to login or sign-up!"
 
-        // Setting up the characteristics of the username and password field!
-        username_text_field.inputType = InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
-        password_text_field.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
-        show_password_check_box.text = "Show Password"
+        // Adding characteristics to second_text_view
+        second_text_view.text = "Just use is directly as a guest!"
 
-        // Assigned listener to this activity.
-        show_password_check_box.setOnClickListener {
-            if (show_password_check_box.isChecked)
-                Log.d("()()()()", "Check box CHECKED!!!")
-            if (!show_password_check_box.isChecked)
-                Log.d("()()()()", "Check box UNCHECKED!!!")
+        // Adding characteristics to use_it_button
+        use_it_button.text = "USE IT"
+        use_it_button.setOnClickListener {
+
         }
 
-
         // Adding main_layout to the screen/window
+        main_layout.addView(first_text_view)
+        main_layout.addView(second_text_view)
         setContentView(main_layout)
 
     }
 
-    
+
 }
--- app/src/main/java/com/myidea/rofy/knowahead/user_util/AppUser.kt
+++ app/src/main/java/com/myidea/rofy/knowahead/user_util/AppUser.kt
@@ -4,7 +4,8 @@ public open class AppUser constructor() {
 
     public var firstName: String? = null
     var lastName: String? = null
-    var phoneNumber: Array<Int>? = null
+    var phoneNumber: Int? = null
     var email: String? = null
+    var password: String? = null
 
 }
